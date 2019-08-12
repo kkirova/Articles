a653457041ef543cf41533ed656e2f0070066422
kkirova <52708975+kkirova@users.noreply.github.com>
2019-08-12 20:16:30 +0300

    Add mongoose db connection

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 const config = require('./config/config');
-const articlesController = require('./controllers/articles-controller');
+const router = require('./config/routes');
 const app = express();
+const db = require('./config/database');
+
+db(config);
 
 app.engine('hbs', handlebars({
     defaultLayout: 'main',
@@ -16,20 +19,8 @@ app.use(bodyParser.urlencoded({
 
 app.set('view engine', 'hbs');
 
+router(app);
 
 app.use(express.static('./public'));
-
-app.get('/', (req, res) => {
-    res.render('home/index');
-});
-
-app.get('/articles/create', articlesController.getCreate);
-
-app.post('/articles/create', articlesController.postCreate);
-
-app.get('/articles', (req, res) => {
-    res.render('articles/index');
-});
-
 
 app.listen(config.port, () => console.log(`Listening on port ${config.port}....`));
